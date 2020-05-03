ec2f9b6e7e375d3a89632f022a54fc1d21b835b8
nitashadhingra <44186440+nitashadhingra@users.noreply.github.com>
2020-05-03 20:46:50 +0530

    day 1

---
+++ firstbadversion.cpp
@@ -0,0 +1,24 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int s = 1;
+        int e = n;
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(!isBadVersion(m))
+                s=m+1;
+            else if(!isBadVersion(m-1))
+                return m;
+            else
+                e=m;
+        }
+        return n;
+    }
+};
+
+// Used Binary Search
+// Time Complexity: O(logn)
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/
